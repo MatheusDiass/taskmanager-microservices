ffbb3f8ec15da4d2ca3eecc2f622b8baf1fe3c4b
Matheus Dias <dias.math0@gmail.com>
2023-10-13 23:45:21 -0300

    refactor(task-microservice): Created http response function to return json and error

--- 1-task-microservice/src/presentation/controllers/create-task.go
+++ 1-task-microservice/src/presentation/controllers/create-task.go
@@ -3,10 +3,10 @@ package controllers
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 	"task-microservice/src/domain/entity"
 	usecases "task-microservice/src/domain/use-cases/create-task"
+	"task-microservice/src/presentation/helpers"
 )
 
 type CreateTaskController struct {
@@ -21,18 +21,18 @@ func (controller CreateTaskController) Handle(w http.ResponseWriter, r *http.Req
 	body, err := io.ReadAll(r.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		helpers.ErrorResponse(w, http.StatusUnprocessableEntity, err)
 	}
 
 	var taskRequest entity.Task
 	if err = json.Unmarshal(body, &taskRequest); err != nil {
-		log.Fatal(err)
+		helpers.ErrorResponse(w, http.StatusBadRequest, err)
 	}
 
 	if err = controller.createTaskUseCase.Execute(
 		taskRequest.Title,
 		taskRequest.Description,
 		taskRequest.UserId); err != nil {
-		log.Fatal(err)
+		helpers.ErrorResponse(w, http.StatusBadRequest, err)
 	}
 }
